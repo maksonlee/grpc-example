bb5c30dd1b53ad09a8ddbcf8cfc4a507bf554736
Makson Lee <cdlee123@gmail.com>
2020-05-29 13:55:03 +0800

    Initial commit
    Change-Id: I33d7bf017e2884a26adf2a70111c5b894f673945

---
+++ csharp/SfisClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using Sfisgrpc;
+using System;
+
+namespace SfisClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+            var client = new Sfis.SfisClient(channel);
+            var reply = client.GetItemStations(new Imei{ Name = "12345678" });
+            
+            Console.WriteLine(reply);
+        }
+    }
+}
---
+++ csharp/SfisServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Sfisgrpc;
+
+namespace SfisServer
+{
+    class SfisImpl : Sfis.SfisBase
+    {
+        public override Task<StationResponse> GetItemStations(Imei request, ServerCallContext context)
+        {
+            List<Station> stations = new List<Station>() { new Station{ Name = "download" }, new Station{ Name = "packing" } };
+            StationResponse response = new StationResponse();
+            response.Stations.Add(stations);
+            return Task.FromResult(response);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Sfis.BindService(new SfisImpl()) },
+                Ports = { new ServerPort("localhost", 50051, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Greeter server listening on port 50051");
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
